bce1b83597b0d079a8593817c7eceef268cacf58
Adekunle2445 <adekunleoyero2021@gmail.com>
2026-09-17 00:54:14 +0100

    to dice roller program

--- index.py
+++ index.py
@@ -1,7 +1,64 @@
-capitals = {"Nigeria" : "Abuja",
-            "USA"     : "Washington",
-            "China"   : "Beingin",
-            "Russia"  : "Mosscow"}
+import random
+dice_art = {
+    1: (
+        "┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘",
+    ),
+    2: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    3: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘",
+    ),
+    4: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    5: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+    6: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘",
+    ),
+}
 
-for count in capitals.keys():
-    print(count)
+while True:
+    num_of_dice = input("How many number of Dice (q to quit) :")
+    if num_of_dice.capitalize() == "Q":
+        print("Thanks for Playing")
+        break
+    elif not num_of_dice.isdigit():
+        print("Only digits allowed")
+        continue
+    else:
+        dice = []
+        num_of_dice = int(num_of_dice)
+        for num in range(num_of_dice):
+            dice.append(random.randint(1, 6))
+
+        print(dice)
+        for die in dice:
+            for line in dice_art[die]:
+                print(line)
